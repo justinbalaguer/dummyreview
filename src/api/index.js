b0b708e79cf3be952610d22992410b03958ee097
Justin Balaguer <justinanthony.primary@gmail.com>
2021-01-02 20:29:44 +0800

    🎉 v1.0.0

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,38 @@
 const express = require('express');
-
-const emojis = require('./emojis');
+const mongoose = require('mongoose');
 
 const router = express.Router();
+const { Schema } = mongoose;
 
-router.get('/', (req, res) => {
-  res.json({
-    message: 'API - 👋🌎🌍🌏'
-  });
+const reviewsSchema = new Schema({
+  fullname: String,
+  picture_url: String
 });
 
-router.use('/emojis', emojis);
+const ReviewsRequest = mongoose.model('ReviewsRequest', reviewsSchema, 'reviews');
+
+router.get('/', async (req, res, next) => {
+  try {
+    mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    const { page = 1, limit = 10 } = req.query;
+    const data = await ReviewsRequest.find()
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .exec();
+
+    const count = await ReviewsRequest.countDocuments();
+    res.json({
+      data,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
--- src/app.js
+++ src/app.js
@@ -5,6 +5,15 @@ const cors = require('cors');
 
 require('dotenv').config();
 
+const rateLimit = require('express-rate-limit');
+
+const apiLimit = rateLimit({
+  windowMs: 24 * 60 * 60 * 1000, // 24 hours
+  max: 1000, // limit of each IP
+  message: 'Uh oh! You have reached the maximum api call (1000 calls per day)',
+  headers: true
+});
+
 const middlewares = require('./middlewares');
 const api = require('./api');
 
@@ -15,13 +24,19 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+app.set('trust proxy', 1);
+
+app.get('/', apiLimit, (req, res) => {
   res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
+    'sample-pagination': '?page=1',
+    'sample-limit': '?limit=10',
+    sample: '?page=1&limit=10'
   });
 });
 
-app.use('/api/v1', api);
+app.use('/api/v1', apiLimit, api);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
